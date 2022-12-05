a3d531423687ac0eb61b084de0ad49ad574c2e0c
Vladislav Pribylov <v.a.pribylov@gmail.com>
2022-12-05 14:46:26 +0300

    string length corrected

--- Practice3.10/Task5/Task5.cs
+++ Practice3.10/Task5/Task5.cs
@@ -10,15 +10,18 @@
             int border = int.Parse(Console.ReadLine());
 
             int randomNumber = randomize.Next(0, border + 1);
-            Console.WriteLine("Число загадно. Вводите числа и попытайтесь угадать его." +
+            Console.WriteLine("Число загадно. " +
+                "Вводите числа и попытайтесь угадать его." +
                 " Чтобы выйти, введите пустую строку");
             while (true)
             {
                 string input = Console.ReadLine();
                 if (input == "") break;
                 int inputNumber = int.Parse(input);
-                if (inputNumber < randomNumber) Console.WriteLine("Число меньше задуманного");
-                else if (inputNumber > randomNumber) Console.WriteLine("Число больше задуманного");
+                if (inputNumber < randomNumber) 
+                    Console.WriteLine("Число меньше задуманного");
+                else if (inputNumber > randomNumber) 
+                    Console.WriteLine("Число больше задуманного");
                 else
                 {
                     Console.WriteLine("Вы угадали!");
