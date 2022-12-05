62b07c1dc8b50e9a283ae81829775ce28b9ce2ea
Vladislav Pribylov <v.a.pribylov@gmail.com>
2022-12-05 14:37:14 +0300

    initial

---
+++ Practice3.10/Task4/Task4.cs
@@ -0,0 +1,19 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minValue = int.MaxValue;
+            Console.WriteLine("Введите количество чисел:\n");
+            int length = int.Parse(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("Введите число:\n");
+                int number = int.Parse(Console.ReadLine());
+                if (number < minValue) minValue = number;
+            }
+            Console.WriteLine($"Наименьшее значение: {minValue}");
+        }
+    }
+}
