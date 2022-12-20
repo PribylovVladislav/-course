8ffd88507789e0c40fbb09f79e42d8bdf5d40282
Vladislav Pribylov <v.a.pribylov@gmail.com>
2022-12-20 23:59:13 +0300

    initial

---
+++ Practice5.5/Practice5.5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence: ");
+            string text = Console.ReadLine();
+            string[] splitted = SplitText(text);
+            NewLines(splitted);
+        }
+
+        static string[] SplitText(string text)
+        {  
+            string[] splitted = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return splitted;
+        }
+
+        static void NewLines(string[] stringArray)
+        {
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                Console.WriteLine(stringArray[i]);
+            }
+        }
+    }
+}
---
+++ Practice5.5/Practice5.5Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Practice5._5Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence: ");
+            string inputPhrase = Console.ReadLine();
+            string[] wordsArray = ReverseWords(inputPhrase);
+
+            string resultString = "";
+            for (int i = 0; i < wordsArray.Length; i++)
+            {
+                resultString += wordsArray[i] + " ";
+            }
+            Console.WriteLine(resultString);
+        }
+        static string[] SplitText(string text)
+        {
+            string[] splitted = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return splitted;
+        }
+
+        static string[] ReverseWords(string inputPhrase)
+        {
+            string[] text = SplitText(inputPhrase);
+            Array.Reverse(text);
+            return text;
+        }
+    }
+}
