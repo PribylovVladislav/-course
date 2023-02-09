ab750c816ae142c9060adcec5d1bcab0fd192851
Vladislav Pribylov <v.a.pribylov@gmail.com>
2023-02-09 23:02:37 +0300

    initial

---
+++ Practice9.5/Practice9.5/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Practice9._5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSplit_Click(object sender, RoutedEventArgs e)
+        {
+            string sentence = InputBox.Text;
+            string[] splitted = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            OutputSplit.ItemsSource= splitted;
+        }
+
+        private void ButtonReverse_Click(object sender, RoutedEventArgs e)
+        {
+            string sentence = InputBoxReverse.Text;
+            string[] splitted = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(splitted);
+            StringBuilder output = new StringBuilder();
+            foreach (string s in splitted)
+            {
+                output.Append(s + " ");
+            }
+            LabelTextBlock.Text = output.ToString();
+        }
+    }
+}
